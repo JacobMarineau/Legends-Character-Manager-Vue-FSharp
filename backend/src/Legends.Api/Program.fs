001a970b1d15a4e636df8499d79bac3451f19ab6
JacobMarineau <jacobmarineaump@gmail.com>
2024-12-17 10:54:31 -0600

    made project

---
+++ backend/src/Legends.Api/Program.fs
@@ -0,0 +1,13 @@
+﻿open Saturn
+open Giraffe
+
+let api = application {
+    use_router (router {
+        get "/" (text "Welcome to Legends Character Manager!")
+    })
+    url "http://localhost:5000"
+}
+
+run api
+
+
